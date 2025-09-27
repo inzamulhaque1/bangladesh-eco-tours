aa2c5c20c9dc8b97e0f8b878e0fe941ce7e94d9f
MD INZAMUL HAQUE JOY <inzamulhaque1002@gmail.com>
2025-09-27 15:49:25 +0600

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import logo from '../assets/Images/logo/eco-tours-logo.png'
+
+const Navbar = () => {
+    return (
+        <div>
+            Navbar
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ src/routes/routes.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home";
+import MainLayout from "../layout/MainLayout";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+
+    ],
+  },
+
+]);
